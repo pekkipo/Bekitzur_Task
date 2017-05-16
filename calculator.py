1dd815626b634c17d161a86c8f1d6f81be824c7f
pekkipo <pekkipodev@gmail.com>
2017-05-16 19:13:57 +0200

    Second

--- calculator.py
+++ calculator.py
@@ -2,12 +2,13 @@
 class Calculator(object):
 
     def __init__(self, equation):
+
         self.equation = equation
         self.numbers_dict = {
-            0: ' ', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'Ten',
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen',
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen', 20: 'twenty',
-            30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty',
+            0: ' ', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
+            20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty',
             70: 'seventy', 80: 'eighty', 90: 'ninety'
         }
 
